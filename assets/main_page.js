32ea5ae19c75d67cabf7bc88df2911aa0fbde169
Paulina Alford <p.t.kaminska@gmail.com>
2026-08-13 16:42:12 +0200

    Projects section: ad styling and fetching from Json - added Handlebars

--- assets/main_page.js
+++ assets/main_page.js
@@ -1,13 +1,59 @@
-
+import Handlebars from 'https://cdn.jsdelivr.net/npm/handlebars@4.7.8/+esm';
 import data from "./data/main_page.json" with { type: "json" };
+import projects_data from "./data/projects.json" with { type: "json" };
 
-function fillData() {
+function fillBioData() {
   const about_me = document.getElementById("about_me");
   const parser = new DOMParser();
   let about_me_content = parser.parseFromString(data["bio"], 'text/html');
   about_me.append(...about_me_content.body.childNodes)
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  fillData();
-});
+function getRandomElements(array, count) {
+  const indices = new Set();          // holds unique index numbers, e.g. {2, 7, 0}
+  const max = Math.min(count, array.length);
+
+  while (indices.size < max) {
+    indices.add(Math.floor(Math.random() * array.length)); // random index 0..length-1
+  }
+
+  return [...indices].map((i) => array[i]); // turn indices into actual project objects
+}
+
+function fillProjectsData() {
+  // return // waiting for more data
+  if (projects_data.length <= 0) return;
+
+  const projects = document.getElementById("projects");
+
+  Handlebars.registerHelper('print_technologies', function (array) {
+    return array.join(', ')
+  });
+
+  const template = Handlebars.compile(`
+    <a href="" class="project">
+      <h3>{{name}}</h3>
+      <div class="image-box">
+        {{#if image}}<img id="{{image}}"/>{{/if}}
+      </div>
+      <div class="lead">{{lead}}</div>
+      <div class="technologies"> {{print_technologies technologies}} </div>
+    </a>`
+  );
+  const parser = new DOMParser();
+
+  const selected_projects = projects_data.length > 3 ? getRandomElements(projects_data, 3) : projects_data
+  selected_projects.forEach((element) => {
+    let project_html = template(element)
+    let project_content = parser.parseFromString(project_html, 'text/html');
+    projects.append(...project_content.body.childNodes)
+
+    if (!element["image"]) return
+
+    let image = document.getElementById(element["image"])
+    image.src = "./assets/images/" + element["image"];
+  });
+}
+
+fillBioData();
+fillProjectsData();
